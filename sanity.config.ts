4b16c2d6365c5fd91d296e344670ac6ee875b065
Mark Rozin <mark.rozin@paktolus.com>
2026-07-04 12:53:33 -0400

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'AF Website',
+
+  projectId: 'xk0vwazc',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
